62c6f22f25e4c10c38efd43f482ea3296b122062
EskinderAka <141957602+EskinderAka@users.noreply.github.com>
2023-11-18 23:28:40 +0300

    re Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,51 +1,54 @@
 #include "main.h"
-
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
+ * _printf - print formated text to output
  *
- * Return: number of chars printed.
+ * @format: the formated string
+ * Return: the no of bytes
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	unsigned int pret = 0, ij, count, di_count = 0;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+	va_list args;
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	va_start(args, format);
+
+	for (ij = 0; format[ij] != '\0'; ij++)
 	{
-		if (format[i] == '%')
+		if (format[ij] != '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
+			my_putchar(format[ij]);
+		}
+		else if (format[ij] == '%' && format[ij + 1] == 'c')
+		{
+			my_putchar(va_arg(args, int));
+			ij++;
+		}
+		else if (format[ij] == '%' && format[ij + 1] == 's')
+		{
+			count = dee_puts(va_arg(args, char *));
+			pret += (count - 1);
+			ij++;
+		}
+		else if (format[ij] == '%' && (format[ij + 1] == '%'))
+		{
+			my_putchar('%');
+			ij++;
+		}
+		else if (format[ij + 1] == 'd' || format[ij + 1] == 'i')
+		{
+			di_count += _putint(va_arg(args, int));
+			ij++;
+			pret += (di_count - 1);
 		}
 		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		{
+			my_putchar('%');
+		}
+		pret += 1;
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+	va_end(args);
+	return (pret);
 }
